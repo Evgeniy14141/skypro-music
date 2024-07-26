7abaef9f5c6e907e655785f8bf9ffbf038cf011d
Evgeniy14141 <eugene-gr14@yandex.ru>
2024-07-26 18:04:58 +0400

    Homework-2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-      <Main tracks={tracks} />
+        <Main tracks={tracks} />
         <Bar />
         <footer className="footer"></footer>
       </div>
